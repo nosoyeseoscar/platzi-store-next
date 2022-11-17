d7280a27d42a56471294da01c1fe6fa65fb1a6f8
Oscar Fuentes <ofuentese@gmail.com>
2022-11-17 08:03:08 -0700

    all ready

--- next.config.js
+++ next.config.js
@@ -1,7 +1,42 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+/* const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    customKey: 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/hola',
+        destination: 'https://gndx.dev',
+        permanent: true,
+      },
+    ];
+  },
+};
+ */
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['api.lorem.space', 'placeimg.com', 'cdn.pixabay.com', 'vuzoon.com', 'www.complementosdelcafe.com', 'images.pexels.com','res.cloudinary.com', 'http2.mlstatic.com'],
+  },
+};
+
+/* const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['api.lorem.space', 'placeimg.com', 'cdn.pixabay.com', 'vuzoon.com', 'www.complementosdelcafe.com'],
+  },
+};
+
+module.exports = nextConfig;
+
+module.exports = {
+  reacStrictMode: true,
+}; */
+
+//module.exports = nextConfig;
